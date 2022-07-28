c7730f30040b9b623f78f73874145e50ed19f4cc
linebot0705 <jim880705@gmail.com>
2022-07-29 03:51:46 +0800

    prac

---
+++ dictionary.py
@@ -0,0 +1,6 @@
+# key and value
+
+dic = {"蘋果":"apple", "香蕉":"banana", "貓":"cat", "狗":"dog"}
+dic2 = {0:"zero", 1:"one", 2:"two"}
+print(dic["蘋果"]) #查詢key(value不能拿來查詢)
+print(dic2[0])
--- if_statement.py
+++ if_statement.py
@@ -1,6 +1,28 @@
 hungry = False
+rainy = True
 
 if hungry:
     print("吃飯")
 else:
     print("不吃飯")
+
+#多個判斷式
+if hungry and not(rainy):
+    print("出門吃飯")
+elif hungry and rainy:
+    print("不出門吃飯")
+elif not(hungry) and not(rainy):
+    print("出門但不吃飯")
+else:
+    print("在家")
+
+#判斷最大數
+num = [10, 20, 30, 5, 43, 100]
+def max_num(num):
+    max = 0
+    for i in range(len(num)):
+        if max < num[i]:
+            max = num[i]
+    return max
+
+print(max_num(num))
